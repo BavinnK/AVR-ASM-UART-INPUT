7a49e6fd53336f463389dd94bda64c4cf1e8afaa
Bavin <bavinbav5@gmail.com>
2025-06-09 15:32:50 +0300

    added internal resistor to enable for the button

--- src/main.cpp
+++ src/main.cpp
@@ -10,13 +10,18 @@ extern "C"{
    void digitalSetASM(uint8_t port,uint8_t bit);
    void digitalClearASM(uint8_t port,uint8_t bit);
    uint8_t UartTransmit(uint8_t transmit);
+   
 
 }
 void setup() {
 
   digitalSetASM(_SFR_IO_ADDR(DDRB),1<<PB5);
-  pinMode(buttonpin,INPUT_PULLUP);
-
+ 
+  digitalClearASM(_SFR_IO_ADDR(DDRB),1<<PB4); // this one when we clear the DDRB is basically makes the D12 pin to input if it was set like the one line code above it would have been output!!
+  digitalSetASM(_SFR_IO_ADDR(PORTB),1<<PB4);// and this one sets the pull up resistor
+  //one other thing for debounce button problem just add 100nF cermaic cap and everything will befine its like 10 cent for each capacitor and we dont wanna eat up CPU cycle for that we just use hardware
+  
+  //pinMode(buttonpin,INPUT_PULLUP);
   Serial.begin(115200);
 
 }
